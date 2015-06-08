45deee3db4997d8e62c55d28224e6488ef597ec0
Andrew Stroup <andrew.m.stroup@gmail.com>
2015-06-08 00:23:27 -0400

    added in initial Google Doc submission code and updated minor tweaks to form

--- js/main.js
+++ js/main.js
@@ -165,4 +165,60 @@ $(document).ready(function() {
       });
     }
   });
+
+  // intake form submission
+  $.ajax({
+    url: "https://docs.google.com/forms/d/1mgnKjMOUHA3jUIONrPoOKcQSxcPQu-nlYcqeUlqEpr0/formResponse",
+    data: { "entry_539610281": name,
+            "entry_2052643685": email,
+            "entry_1717830975": position,
+            "entry_441930163": role,
+            "entry_1059845155": cto,
+            "entry_984976714": cto_contact,
+            "entry_2015518157": priority,
+            "entry_1896597527": problem,
+            "entry_1910247938": solution,
+            "entry_349657906": impact,
+            "entry_1140645356": data,
+            "entry_103429177": skill,
+            "entry_737405442": domain,
+            "entry_757989308": number,
+            "entry_1329682796": sponsor,
+            "entry_1622924413": length },
+    type: "POST",
+    dataType: "xml",
+    statusCode: {
+      0: function () {
+        // Mixpanel
+        mixpanel.identify(email);
+        mixpanel.people.set({
+          '$email': email,
+          '$Wait List': true
+        });
+        mixpanel.track("Middle CTA Button");
+
+        $('#waitEmail2').parent().removeClass('has-success').removeClass('has-feedback');
+        $('#wait-glyph-success-2').css('visibility', 'hidden');
+        document.getElementById("waitEmail2").value = '';
+        $('#waitModal').modal();
+      },
+      200: function () {
+        // Mixpanel
+        mixpanel.identify(email);
+        mixpanel.people.set({
+          '$email': email,
+          '$Wait List': true
+        });
+        mixpanel.track("Middle CTA Button");
+
+        $('#waitEmail2').parent().removeClass('has-success').removeClass('has-feedback');
+        $('#wait-glyph-success-2').css('visibility', 'hidden');
+        document.getElementById("waitEmail2").value = '';
+        $('#waitModal').modal();
+      }
+    }
+  });
+  setTimeout(function () {
+    $('#waitButton2').prop("disabled", false).html('Increase Your Odds');
+  }, 1000);
 });
