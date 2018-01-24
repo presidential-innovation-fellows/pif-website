8d7e72b52a7a7a9ca39aa60eb20f258554953971
MJ Rossetti <michael.rossetti@gsa.gov>
2018-01-24 13:12:42 -0500

    Generate pif counts by region, fix unrecognized regions

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 gem 'jekyll'
 gem 'jekyll-sitemap'
+gem 'activesupport'
 
 group :test do
   gem 'rake'
--- _plugins/joiner.rb
+++ _plugins/joiner.rb
@@ -1,8 +1,12 @@
+require 'active_support/core_ext/hash/keys' # enable Hash#symbolize_keys
+require 'pp' # enable pretty printing
+
 module Pif
   class Joiner
     def self.join_data(site)
       filter_agencies(site)
       consolidate_pif_projects_by_agency(site)
+      #log_fellow_counts(site) # un-comment this line to print fellow and region counts to the server logs (for example if you need to update the homepage map :-)
     end
 
     def self.filter_agencies(site)
@@ -42,6 +46,23 @@ module Pif
       projects.concat site.data['current_projects'].values
       projects.concat site.data['previous_projects'].values
       site.data['projects'] = projects
+    end
+
+    # count the number of fellows, and number of fellows per region
+    # ... and print results to server logs
+    # ... adapted from source: https://stackoverflow.com/a/13519616/670433
+    def self.log_fellow_counts(site)
+      fellows = site.data['fellows']
+
+      fellow_regions = fellows.map{|_,v| v['region']} #> ["northeast", "northeast", "west", "midwest", etc.]
+      raise 'UNASSIGNED REGION(S) ERROR!' if fellow_regions.include?(nil)
+
+      region_counts = fellow_regions.group_by{|region| region.downcase}.map{|k,v| [k, v.count] } #> [["northeast", 40], ["west", 45], etc.]
+      region_counts = Hash[*region_counts.flatten].symbolize_keys #> {northeast: 40, west: 45, etc.}
+      raise 'UNRECOGNIZED REGION(S) ERROR!' if region_counts.keys.sort != [:midwest, :northeast, :outside, :south, :west]
+
+      fellow_counts = {total: fellows.count, by_region: region_counts}
+      pp 'FELLOW COUNTS', fellow_counts
     end
   end
 end
