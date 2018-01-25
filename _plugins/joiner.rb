7313c82132994b24f0ea55a5c090eff83d9acc30
MJ Rossetti <s2t2@users.noreply.github.com>
2018-01-25 12:14:32 -0500

    Update Map (#173)
    * Update hard-coded counts
    
    * Generate pif counts by region, fix unrecognized regions
    
    * Update map
    
    * Run in custom env to avoid federalist errors
    
    * Actually activesupport was not that necessary here
    
    * Conditionally require b/c maybe this is also breaking the federalist build
    
    * Remove this test I never really set up
    
    * Maybe even a conditional require breaks the build
    
    * Update docs and be less cautious about breaking federalist build
    
    * Address PR review comments

--- _plugins/joiner.rb
+++ _plugins/joiner.rb
@@ -1,8 +1,11 @@
+require 'pp'
+
 module Pif
   class Joiner
     def self.join_data(site)
       filter_agencies(site)
       consolidate_pif_projects_by_agency(site)
+      log_counts(site) if ENV.fetch('JEKYLL_ENV', 'development') == 'dev_logging' # only run in this custom environment (should not be run on federalist, which uses a default JEKYLL_ENV of "development")
     end
 
     def self.filter_agencies(site)
@@ -42,6 +45,29 @@ module Pif
       projects.concat site.data['current_projects'].values
       projects.concat site.data['previous_projects'].values
       site.data['projects'] = projects
+    end
+
+    # count the number of agencies, fellows, and fellows per region
+    # ... and print the results
+    # ... adapted from source: https://stackoverflow.com/a/13519616/670433
+    def self.log_counts(site)
+      agencies = site.data['agencies']
+      fellows = site.data['fellows']
+
+      fellow_regions = fellows.map{|_,v| v['region'].to_sym} #> ["northeast", "northeast", "west", "midwest", etc.]
+      raise 'UNASSIGNED REGION(S) ERROR!' if fellow_regions.include?(nil) #TODO: move me to a test
+
+      region_counts = fellow_regions.group_by{|region| region.downcase}.map{|k,v| [k, v.count] } #> [["northeast", 40], ["west", 45], etc.]
+      region_counts = Hash[*region_counts.flatten] #> {northeast: 40, west: 45, etc.}
+      raise 'UNRECOGNIZED REGION(S) ERROR!' if region_counts.keys.sort != [:midwest, :northeast, :outside, :south, :west] #TODO: move me to a test
+
+      counts = {
+        agencies: agencies.count,
+        fellows: fellows.count,
+        fellows_by_region: region_counts
+      }
+
+      pp 'COUNTS', counts
     end
   end
 end
