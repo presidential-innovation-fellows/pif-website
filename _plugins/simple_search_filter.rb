d8a426b7bfa24b501cd2f626833fab80c788ddeb
Jeff K <jeffery.keene@gsa.gov>
2026-06-09 10:46:17 -0400

    fixed backslash for codeql

--- _plugins/simple_search_filter.rb
+++ _plugins/simple_search_filter.rb
@@ -1,8 +1,8 @@
 module Jekyll
   module CharFilter
     def remove_chars(input)
-      input.gsub! '\\','&#92;'
-      input.gsub! /\t/, '    '
+      input.gsub!(/\\/, '&#92;')
+      input.gsub!(/\t/, '    ')
       input.strip_control_and_extended_characters
     end
   end
